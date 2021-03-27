109f6f813aacfc7396983e0547bdb13bf178a73c
Crizj <JieZhengNJU@outlook.com>
2021-03-27 09:10:31 +0800

    improved code

--- leetcode/edit-distance.cpp
+++ leetcode/edit-distance.cpp
@@ -13,48 +13,33 @@ class Solution
 public:
     int minDistance(string word1, string word2) 
     {
-        vector<vector<int>> result(word1.size(),vector<int>(word2.size(),0));
-        
-        for(int i = 0; i < word1.size();i++)
+        vector<vector<int>> result(word1.size()+1,vector<int>(word2.size()+1,0));
+
+        for (int i = 1; i <= word1.size(); i++) 
         {
-            for(int j = 0; j < word2.size(); j++)
+            result[i][0] = i;
+        }
+        for (int j = 1; j <= word2.size(); j++) 
+        {
+            result[0][j] = j;
+        }
+        for(int i = 1; i <= word1.size();i++)
+        {
+            for(int j = 1; j <= word2.size(); j++)
             {
                 if(word1[i] == word2[j])
                 {
-                    if(i-1 >= 0 && j-1 >= 0)
-                    {
-                        result[i][j] = result[i-1][j-1];
-                    }
-                    else
-                    {
-                        result[i][j] = max(i,j);
-                    }
+                    result[i][j] = result[i-1][j-1];
                 }
                 else
                 { 
-                    if(i == 0 && j == 0) result[i][j] = 1;
-                    else if(i == 0) result[i][j] = result[i][j-1] + 1;
-                    else if(j == 0) result[i][j] = result[i-1][j] + 1;
-                    else 
-                    {
-                        result[i][j] = min(result[i-1][j-1]+1,result[i-1][j]+1);
-                        result[i][j] = min(result[i][j],result[i][j-1]+1);
-                    }
+                    result[i][j] = min(result[i-1][j-1]+1,result[i-1][j]+1);
+                    result[i][j] = min(result[i][j],result[i][j-1]+1);
                 }
             }
         }
-        if(word1.empty())
-        {
-            cout << word2.size() << endl;
-            return word2.size();
-        }
-        else if(word2.empty())
-        {
-            cout << word1.size() << endl;
-            return word1.size();
-        }
-        cout << result[word1.size()-1][word2.size()-1] << endl;
-        return result[word1.size()-1][word2.size()-1];
+        cout << result[word1.size()][word2.size()] << endl;
+        return result[word1.size()][word2.size()];
     }
 };
 
